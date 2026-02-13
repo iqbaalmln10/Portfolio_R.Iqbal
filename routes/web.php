9169ebeaf45570efeda3cb0a1d8a72d189c56a96
iqbaalmln10 <r.iqbal.maulana.ibrahim@gmail.com>
2026-02-13 22:55:24 +0700

    coba coba

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
-Route::get('/', [PortfolioController::class, 'index'])->name('portfolio');
-Route::get('/project/{id}', [PortfolioController::class, 'show'])->name('project.show');
+// Route::get('/', [PortfolioController::class, 'index'])->name('portfolio');
+Route::get('/', [PortfolioController::class, 'show'])->defaults('id', '1');
 
 
 
